62fc4afb752f7ff4b47ef145ce8b82358c7d98ba
DaShinja <byron@dashinja.com>
2019-06-01 01:19:18 -0400

    Challenge question completed: Sum of integers divisble by 3 in the range 1 through 20

--- Program.cs
+++ Program.cs
@@ -41,9 +41,42 @@ namespace BranchesAndLoops
       }
     }
 
+    static void Loopy()
+    {
+      // While Loop
+      int counter = 0;
+      while (counter < 10)
+      {
+        Console.WriteLine($"Hello World! The counter is {counter}");
+        counter++;
+      }
+
+      // Do While loop
+      counter = 0;
+      do
+      {
+        Console.WriteLine($"Hello World! The counter is {counter} and I'm a do while Loop!");
+        counter++;
+      } while (counter < 10);
+
+      // For Loop
+      for (int index = 0; index < 10; index++)
+      {
+        Console.WriteLine($"Hello WOrld! The index is {index}");
+      }
+    }
+
     static void Main(string[] args)
     {
-      ExploreIf();
+      int accum = 0;
+      for (int i = 1; i <= 20; i++)
+      {
+        if (i % 3 == 0)
+        {
+          accum = accum + i;
+        }
+      }
+      Console.WriteLine($"The sum of all integers 1 through 20 divisible by 3 is {accum}!");
     }
   }
 }
